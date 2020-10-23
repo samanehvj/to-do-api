a70d0b8087fb12406c343e7bcfe8b125503befd7
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-10-22 18:25:28 -0700

    Update list of data.json

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('I am learning Node');
+// console.log('I am learning Node');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let data = require('./data.json');
+
+app.listen(port, () => {
+    console.log('app started on port: ' + port);
+});
+
+app.get('/', (req, res) => {
+    let response = {
+        todos: data
+    };
+    res.send(response);
+});
