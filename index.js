244e021b1f1a98feaa6a647ad23c0f752bb11a45
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-10-29 12:39:59 -0700

    Add new getTodos and addTodo function in index.js

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-// console.log('I am learning Node');
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-let data = require('./data.json');
+app.use(express.json());
 
-app.listen(port, () => {
-    console.log('app started on port: ' + port);
-});
-
-app.get('/', (req, res) => {
-    res.send('Hi, I am Samaneh Vajdi. I use Node and Express for serving this content');
-});
-
-app.get('/data', (req, res) => {
-    let response = {
-        todos: data
-    };
+//Get all TODO
+function getTodos(req, res){
+    //Get Data
+    let data = require("./data.json");
+    let response = { todos: data};
     res.send(response);
-})
+}
+
+//Add new TODO
+function addTodo(req, res){
+    //Get Data
+    let data = require("./data.json");
+    console.log("Add TODO Request Comming In... ");
+
+    //Get all TODO ID
+    let allIds = data.map(function (todo) {
+        return todo.id;
+    });
+
+    //Get last id + 1
+    let maxId = Math.max.apply(Math, allIds) + 1;
+
+    //Create new TODO object 
+    let newTodo = {
+        completed:req.body.completed,
+        id: maxId,
+        name:req.body.name,
+    }
+
+    //Add newTodo to data
+    data.push(newTodo);
+
+    //Return new response
+    getTodos(req, res);
+
+}
+
+//Samaneh Vajdi Greeting for Nathan Homework
+function svGreeting(req, res)  {
+    res.send('Hi, I am Samaneh Vajdi. I use Node and Express for serving this content');
+}
+
+app.get('/', svGreeting);
+
+app.get("/todo/", getTodos);
+app.post("/todo/add", addTodo);
+
+
+function appStart(){
+    console.log("To-Do-Api listening at "+ port);
+}
+app.listen(port, appStart);
